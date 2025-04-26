e9522f390baf475ad05d94fe219bfd1900a8485a
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2025-04-26 16:58:52 -0300

    Se creo la clase Libro con sus atributos, getes y seter. Ademas de un enum para sus estados

---
+++ src/main/java/enums/Estado.java
@@ -0,0 +1,6 @@
+package enums;
+
+public enum Estado {
+    DISPONIBLE,
+    PRESTADO
+}
---
+++ src/main/java/modelos/Libro.java
@@ -0,0 +1,45 @@
+package modelos;
+
+import enums.Estado;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private Estado estado;
+
+    public Libro(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.estado = Estado.DISPONIBLE;
+    }
+    // Getters y Setters
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+    public void SetEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
